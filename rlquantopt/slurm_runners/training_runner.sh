6ccedc66112fe3d887f5300162b4267c06a0ff4e
Leander Grech <grechleander@gmail.com>
2024-07-09 22:48:01 +0200

    Updated slurm runner in [rlquantopt/slurm_runners/training_runner.sh]

--- rlquantopt/slurm_runners/training_runner.sh
+++ rlquantopt/slurm_runners/training_runner.sh
@@ -14,46 +14,92 @@
 # SBATCH --mail-type=all
 #
 echo Running on $(hostname)
-scontrol --details show jobs $SLURM_JOBID |grep RES
-env | grep CUDA
 
-source /opt/conda/etc/profile.d/conda.sh
-ENV_NAME=rlquantopt
-VENV=/opt/local/data/lgrec12/.conda/envs/$ENV_NAME
+# Initialize variable to indicate slurm is set by default
+slurm_set=true
 
-USER_DIR=/opt/users/lgrec12
-PROJ_DIR=$USER_DIR/rlquantopt_workspace/rlquantopt
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
 
-if [ -d $VENV ]; then
-	conda activate $ENV_NAME
-	echo Conda environment $ENV_NAME activated
+    --no-slurm)
+      slurm_set=false
+      ;;
+    
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# Check if --no-slurm was set
+if [ "$slurm_set" = true ]; then
+    echo "Slurm mode: preparing environment..."
+
+    # Setup conda environment from requirements.txt if it doesn't exist already
+    ENV_NAME=rlquantopt
+    USER_DIR=/opt/users/lgrec12
+    CONDA_DIR=/opt/local/data/lgrec12/.conda/envs
+    PROJ_DIR=$USER_DIR/rlquantopt_workspace/rlquantopt
+    PYTHON=python
+
+    # Show some details
+    scontrol --details show jobs $SLURM_JOBID |grep RES
+    env | grep CUDA
+
+    source /opt/conda/etc/profile.d/conda.sh
+    VENV=$CONDA_DIR/$ENV_NAME
+
+    if [ -d $VENV ]; then
+        conda activate $ENV_NAME
+        echo Conda environment $ENV_NAME activated
+    else
+        echo Virtual environment $ENV_NAME NOT found
+        conda create --name $ENV_NAME python=3.10
+        conda activate $ENV_NAME
+        conda install pip
+    fi
+
+    # Install project dependencies
+    REQ_PATH=$PROJ_DIR/requirements.txt
+    echo Updating requirements from $REQ_PATH
+    pip install -r $REQ_PATH
+
+    # Install RLQuantOpt package in editable mode
+    pip uninstall rlquantopt
+    pip install -e $PROJ_DIR
 else
-	echo Virtual environment $ENV_NAME NOT found
-	conda create --name $ENV_NAME python=3.10
-	conda activate $ENV_NAME
-	conda install pip
+    echo "Slurm is not set, doing nothing."
 fi
-REQ_PATH=$PROJ_DIR/requirements.txt
-echo Updating requirements from $REQ_PATH
-pip install -r $REQ_PATH
-pip uninstall rlquantopt
-pip install -e $PROJ_DIR
 
+# Training script
 SCRIPT_PATH=$PROJ_DIR/rlquantopt/rl_agents/train_zcqpee_sb3.py
 
-PULSE_LENGTHS=(500 1000 3000 60000)
+# Grid-search parameters
+PULSE_LENGTHS=(500 1000 3000 6000)
 A_NORM_MAXS=(2 5 10)
 ACTION_SCALES=(1e-1 1)
-SEEDS=(123 234 345 456 567)
 T=300
+FID_THRESH=0.995
+SEEDS=(123 234 345 456 567)
 
+# Training parameters
+N_TRAIN=500000
+SAVE_FREQ=1000
+EVAL_FREQ=500
+LOG_INTERVAL=100
+
+# Grid-search
 for pl in "${PULSE_LENGTHS[@]}"; do
-  for amax in "${A_NORM_MAXS[@]}"; do
-    for ascale in "${ACTION_SCALES[@]}"; do
-      for seed in "${SEEDS[@]}"; do
-        python $SCRIPT_PATH --n-train 500000 --log-interval 100 --save-freq 1000 --eval-freq 500 -T $T --pulse-length $pl  --a-norm-max $amax --a-scale $ascale --seed $seed
-      done
+    for a_max in "${A_NORM_MAXS[@]}"; do
+        for a_scale in "${ACTION_SCALES[@]}"; do
+            for seed in "${SEEDS[@]}"; do
+                $PYTHON $SCRIPT_PATH --n-train $N_TRAIN --save-freq $SAVE_FREQ --eval-freq $EVAL_FREQ --log-interval $LOG_INTERVAL \
+                --pulse-length $pl --a-norm-max $a_max --a-scale $a_scale -T $T --fid-thresh $FID_THRESH --seed $seed
+            done
+        done
     done
-  done
 done
 
